77d77109746ddbd30bef20f5c2fb682aa44a88ed
Colesnic Dmitrii <dmitrii.colesnic.01@gmail.com>
2021-06-22 18:29:31 +0300

    The end of the 7-th lesson(Finish update)

--- src/main/java/com/itproger/WebJava/G/D/Controllers/BlogController.java
+++ src/main/java/com/itproger/WebJava/G/D/Controllers/BlogController.java
@@ -62,4 +62,16 @@ public class BlogController {
         return "blog-edit";
     }
 
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostAdd(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow(IllegalStateException::new);
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+
+
 }
